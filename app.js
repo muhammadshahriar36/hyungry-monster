6191ee2c919cd5f2711933b288f86987ff0a57e9
muhammadshahriar36 <muhammadshahriar36@gmail.com>
2021-02-08 23:36:24 +0600

    add code

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ const searchNames = () =>{
 
 const displayNames = names =>{
     const mealContainer = document.getElementById('meal-container');
+    
 
     names.forEach(name => {
         const mealDiv = document.createElement('div');
         mealDiv.className = 'single-result row align-items-center my-3 p-3';
 
-        mealDiv.innerHTML = `
+
+        mealDiv.innerHTML =  `
                 <div class="col-md-9">
                     <img onclick="displayMealDetail('${name.strMeal}')" id="image" src="${name.strMealThumb}">
-                    <h3 class="lyrics-name">${name.strMeal}</h3>
+                    <h3 class="name">${name.strMeal}</h3>
                     <p class="author lead"> <span></span></p>
                 </div>
                 <div class="col-md-3 text-md-right text-center">
-                    <button class="btn btn-success"></button>
+                    
                 </div>
         `;
         mealContainer.appendChild(mealDiv);
@@ -36,18 +38,18 @@ const displayMealDetail = name => {
 }
 
 const renderMealInfo = name => {
-    // console.log(name)
     const mealDiv = document.getElementById('meal-detail');
-    mealDiv.innerHTML = `
+    mealDiv.innerHTML =  `
     <img id="show-image" src="${name.strMealThumb}">
     <h1>${name.strMeal}</h1>
-    <p>${name.strIngredient1}</p>
-    <p>${name.strIngredient2}</p>
-    <p>${name.strIngredient3}</p>
-    <p>${name.strIngredient4}</p>
-    <p>${name.strIngredient5}</p>
-    <p>${name.strIngredient6}</p>
-    <p>${name.strIngredient7}</p>
+    <h3>Ingredients:</h3>
+    <p>1. ${name.strIngredient1}</p>
+    <p>2. ${name.strIngredient2}</p>
+    <p>3. ${name.strIngredient3}</p>
+    <p>4. ${name.strIngredient4}</p>
+    <p>5. ${name.strIngredient5}</p>
+    <p>6. ${name.strIngredient6}</p>
+    <p>7. ${name.strIngredient7}</p>
     `;
 }
 
